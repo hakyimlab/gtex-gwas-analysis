f0d7de757bc451a679da386fdeb4c11b2741f1fa
rbonazzola <rodbonazzola@gmail.com>
2019-06-14 15:30:41 -0500

    GWAS/eQTL correlation calculation with ENLOC RCP threshold

---
+++ code/gwas_eqtl_correlation_with_rcp_threshold.R
@@ -0,0 +1,94 @@
+suppressPackageStartupMessages({
+  library(tidyverse)
+  library(yaml)
+  library(argparse)
+})
+
+parser <- ArgumentParser()
+parser$add_argument("--tissue", default="Whole_Blood")
+parser$add_argument("--phenotype", default="Astle_et_al_2016_Eosinophil_counts")
+parser$add_argument("--rcp_thresholds", default=c(0,0.01, 0.05, 0.1, 0.2, 0.5))
+args <- parser$parse_args()
+
+tissue <- args$tissue
+phenotype <- args$phenotype
+
+# metadata <- read.table("data/gwas_metadata.txt", header=TRUE, stringsAsFactors=FALSE)
+metadata <- read_tsv("data/gwas_metadata.txt")
+dapg_file_pattern <- "data/dapg_variants/with_mashr_results/DAPG_with_mashr__{tissue}.rds"
+enloc_file_pattern <- "data/enloc/{tissue}_w_{short_phenotype}_enloc_output.txt.gz"
+ld_file_pattern <- "data/dapg_variants/eqtl_data/LD/dapg_filterd_LD_{tissue}.txt"
+output_file_pattern <- "output/eqtl_gwas_correlation/{phenotype}__{tissue}__correlation.txt"
+
+short_phenotype <- as.character(metadata[metadata$Tag==phenotype, "new_Phenotype"])
+
+# ------------------------------------------------------------------------------------------ #
+
+dd <- readRDS(glue::glue(dapg_file_pattern))[[phenotype]]
+rcp_df <- read.table(glue::glue(enloc_file_pattern), header=T)
+ld_df <- read.table(glue::glue(ld_file_pattern), header=T)
+
+# gtex_tissue_metadata <-
+#   basicQuery(gtex_tissue_metadata_tbl) %>%
+#   select(tissue, v8_all, tissue_abbrv) %>%
+#   rename(sample_size=v8_all)
+
+dd_gt_1 <- dd %>%
+  group_by(tissue, phenotype, gene_id) %>%
+  filter(n() > 1) %>%
+  mutate(ranking=order(order(abs(eqtl_effect_size), decreasing=TRUE))) %>%
+  ungroup() %>%
+  mutate(beta_gene=gwas_effect_size/eqtl_effect_size) %>%
+  select(gene_id, gtex_variant_id, ranking, beta_gene) %>%
+  filter(ranking <= 2)
+
+dd_wide <-
+  inner_join(
+    dd_gt_1 %>% filter(ranking==1),
+    dd_gt_1 %>% filter(ranking==2),
+    by="gene_id"
+  ) %>%
+  inner_join(
+    ld_df,
+    by=c("gtex_variant_id.x"="SNP1", "gtex_variant_id.y"="SNP2")
+  ) %>%
+  inner_join(
+    rcp_df %>% mutate(gene_id=substr(gene_id, 1, 15)),
+    by="gene_id"
+  ) %>%
+  filter(
+    beta_gene.x > quantile(beta_gene.x, 0.05, na.rm = T) &
+    beta_gene.y > quantile(beta_gene.y, 0.05, na.rm = T) &
+    beta_gene.x < quantile(beta_gene.x, 0.95, na.rm = T) &
+    beta_gene.y < quantile(beta_gene.y, 0.95, na.rm = T)
+  )
+
+rows <- list()
+for (rcp_threshold in args$rcp_thresholds) {
+  dd_rcp <- dd_wide %>% filter(rcp > rcp_threshold)
+  rows <- c(
+    rows, 
+    list(
+      data.frame(
+        "tissue"=tissue, 
+        "phenotype"=phenotype, 
+        "rcp_threshold"=rcp_threshold,
+        "corr_pearson"=cor(dd_rcp$beta_gene.x, dd_rcp$beta_gene.y, use="complete.obs", method="pearson"), 
+        "corr_spearman"=cor(dd_rcp$beta_gene.x, dd_rcp$beta_gene.y, use="complete.obs", method="spearman"),
+        "n"=nrow(dd_rcp),
+        "ld_median"=median(dd_rcp$r2, na.rm=T),
+        "ld_1st_quartile"=quantile(dd_rcp$r2, 0.25, na.rm=T),
+        "ld_3rd_quartile"=quantile(dd_rcp$r2, 0.75, na.rm=T)
+      )
+    )
+  )
+}
+
+output_df <- bind_rows(rows)
+output_filename <- glue::glue(output_file_pattern)
+
+if (!dir.exists(dirname(output_filename))) {
+  dir.create(dirname(output_filename), recursive = TRUE)
+}
+
+write.table(output_df, file = output_filename, quote = FALSE, sep = "\t", row.names = FALSE)
